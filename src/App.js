574826dd3b22132ade4fc2ef5148dc6b17b7f141
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-02-07 18:44:01 +0000

    feat: create users list to display added users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import UserForm from "./components/UserForm";
+import UserList from "./components/UserList";
 
 function App() {
   const [users, setUsers] = useState([]);
   const onUserAdd = (user) => {
     setUsers([...users, user]);
   };
-  return <UserForm onUserAdd={onUserAdd} />;
+  return (
+    <div>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
